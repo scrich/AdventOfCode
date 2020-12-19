89215789a62f0ed6c872335c9dadc0fbb49395c5
scrich <rich@scrich.co.uk>
2020-12-19 02:10:21 +0000

    solved day 18

--- js/18/day18_2.js
+++ js/18/day18_2.js
@@ -2,10 +2,10 @@ puzzles = input.split(/\n/);
 let total = 0;
 
 for (let puzzle of puzzles) {
-    console.log("Calculate PUZZLE "+puzzle);
+    console.log("Calculate PUZZLE " + puzzle);
 
     let answer = calculate(puzzle);
-    console.log("PUZZLE ########## "+puzzle);
+    console.log("PUZZLE ########## " + puzzle);
     console.log("Result: " + answer);
     total += answer;
 }
@@ -14,13 +14,13 @@ document.write("<h2>Total homework</h2>");
 document.write(total);
 
 function calculate(puzzle) {
-    
-    // console.log("called calculate with "+ puzzle);
+
+    console.log("called calculate with "+ puzzle);
     // work out 
-  
+
     let match;
     let result;
-// 
+    // 
     match = puzzle.match(/^\d+$/);
     if (match) {
         // just a single number
@@ -30,20 +30,28 @@ function calculate(puzzle) {
         // console.log("Returning " + result);
         return result;
     }
-    
 
-
-    match = puzzle.match(/^\d+ [\+\*] \d+$/);
+    match = puzzle.match(/^\d+ [\+] \d+$/);
     if (match) {
-        // we can evaluate completely
+        // addition
         // console.log("can evaluate fully");
         result = eval(match[0]);
         // console.log("Returning " + result);
         return result;
+    } else {
+
+        match = puzzle.match(/^\d+ [\*] \d+$/);
+        if (match) {
+            // multiplication
+            // console.log("can evaluate fully");
+            result = eval(match[0]);
+            // console.log("Returning " + result);
+            return result;
+        }
     }
 
     // let match = puzzle.match(/\(.+\)/g);
-    
+
     match = puzzle.match(/\([\d\s\+*]+?\)/);
     if (match) {
         // we have some brackets
@@ -55,20 +63,33 @@ function calculate(puzzle) {
         // console.log("foo: " + foo);
         // console.log("Process " + match[0].slice(1, -1));
         result = calculate(puzzle.slice(0, brackstart) + calculate(match[0].slice(1, -1)) + puzzle.slice(brackend,));
-        
-        
+
+
     } else {
         // console.log("no brackets");
         // process the first two terms
 
-        match = puzzle.match(/^\d+ [\+\*] \d+/);
+        match = puzzle.match(/\d+ [\+] \d+/);
         if (match) {
-            // console.log(match);
+            console.log(match);
             // console.log(`Found ${match[0]} start=${match.index} end=${match.index + match[0].length}.`);
             let foo = match[0] + puzzle.slice(match.index + match[0].length,);
             // console.log("foo: " + foo);
             // console.log("Process "+match[0]);
-            result = calculate(calculate(match[0]) + puzzle.slice(match.index + match[0].length,));
+            result = calculate(puzzle.slice(0,match.index) + calculate(match[0]) + puzzle.slice(match.index + match[0].length,));
+        } else {
+            // console.log("no brackets");
+            // process the first two terms
+
+            match = puzzle.match(/\d+ [\*] \d+/);
+            if (match) {
+                // console.log(match);
+                // console.log(`Found ${match[0]} start=${match.index} end=${match.index + match[0].length}.`);
+                let foo = match[0] + puzzle.slice(match.index + match[0].length,);
+                // console.log("foo: " + foo);
+                // console.log("Process "+match[0]);
+                result = calculate(puzzle.slice(0,match.index) + calculate(match[0]) + puzzle.slice(match.index + match[0].length,));
+            }
         }
     }
     return result;
